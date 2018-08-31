824072468f1d88548a9b615524ceba8f1855e568
Krittanan Pingclasai <kp@kpping.me>
2018-08-31 20:57:07 +0700

    lint happy

--- index.spec.js
+++ index.spec.js
@@ -2,14 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./index");
 it('should get true (null)', () => {
-    expect(index_1.isNil(null))
-        .toBe(true);
+    expect(index_1.isNil(null)).toBe(true);
 });
 it('should get true (undefined)', () => {
-    expect(index_1.isNil(undefined))
-        .toBe(true);
+    expect(index_1.isNil(undefined)).toBe(true);
 });
 it('should get false (other)', () => {
-    expect(index_1.isNil('null'))
-        .toBe(false);
+    expect(index_1.isNil('null')).toBe(false);
 });
--- index.spec.ts
+++ index.spec.ts
@@ -1,16 +1,13 @@
 import { isNil } from './index';
 
 it('should get true (null)', () => {
-    expect(isNil(null))
-        .toBe(true);
+    expect(isNil(null)).toBe(true);
 });
 
 it('should get true (undefined)', () => {
-    expect(isNil(undefined))
-        .toBe(true);
+    expect(isNil(undefined)).toBe(true);
 });
 
 it('should get false (other)', () => {
-    expect(isNil('null'))
-        .toBe(false);
+    expect(isNil('null')).toBe(false);
 });
